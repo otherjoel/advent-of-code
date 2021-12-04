dbd9c689ae60846aa1b6d19e77070c2a85566f47
Joel Dueck <joel@jdueck.net>
2021-12-03 23:21:30 -0600

    day 3: two stars

---
+++ 2021/day03.rkt
@@ -0,0 +1,70 @@
+#lang racket/base
+
+(require "aoc.rkt")
+
+(define line-count (/ (file-size "day03.txt") 13))
+(define halfway (/ line-count 2))
+(define cols (make-vector 12 0))
+
+(define (read-cols! [rem (- line-count 1)] [col 0] [char (read-byte)])
+  (cond
+    [(eof-object? char) (void)]
+    [(eq? char 10) (read-cols! (- rem 1) 0)] ; 10 = newline
+    [else
+     (define col-val (vector-ref cols col))
+     (cond
+       [(char? col-val) (read-cols! rem (+ col 1))]
+       [else
+        (define sum (+ col-val (- char 48)))
+        (define result
+          (cond [(> sum halfway) #\1] ; when 1s have already won
+                [(< (+ sum rem) halfway) #\0] ; when 0s have already won
+                [else sum]))
+        (vector-set! cols col result)
+        (read-cols! rem (+ col 1))])]))
+
+(define (calc)
+  (define-values (gamma epsilon)
+    (for/fold ([g-accum 0]
+               [e-accum 0])
+              ([c (in-vector cols)]
+               [i (in-range 11 -1 -1)])
+      (values (+ g-accum (* (if (eq? c #\1) 1 0) (expt 2 i)))
+              (+ e-accum (* (if (eq? c #\1) 0 1) (expt 2 i))))))
+  (* gamma epsilon))
+
+(define (part-1)
+  (with-input-from-file "day03.txt" read-cols!)
+  (calc))
+
+(module+ test
+  (check-answer part-1 1307354))
+
+(require racket/file
+         racket/math)
+
+(define input (map (lambda (s) (string->number s 2)) (file->lines "day03.txt")))
+
+(define (bit-at num pos)
+  (sgn (bitwise-and num (expt 2 (- pos 1)))))
+
+(define most >=)
+(define least <)
+(define (common-bitval comp lst pos)
+  (if (comp (apply + (map (lambda (n) (bit-at n pos)) lst))
+            (/ (length lst) 2))
+      1 0))
+
+(define (filter-numbers comp)
+  (let loop ([lst input]
+             [pos 12])
+    (define filter-bit (common-bitval comp lst pos))
+    (define remaining (filter (lambda (n) (eq? (bit-at n pos) filter-bit)) lst))
+    (displayln remaining)
+    (if (null? (cdr remaining))
+        (car remaining)
+        (loop remaining (- pos 1)))))
+
+(define (part-2)
+  (* (filter-numbers most)
+     (filter-numbers least)))
