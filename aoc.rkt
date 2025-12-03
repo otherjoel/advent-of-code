e4ae68a5808a2da270519ed3a50ec3d0cbd9064d
Joel Dueck <joel@jdueck.net>
2025-12-03 07:02:51 -0600

    Update aoc.rkt

--- aoc.rkt
+++ aoc.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(require (for-syntax racket/base))
+(require (for-syntax racket/base syntax/parse))
 (require monotonic rackunit)
 
 (provide (all-defined-out)
@@ -14,15 +14,31 @@
         #,(syntax/loc stx (check-equal? (car results) answer))
         (displayln (format "~a: ~a (cpu: ~a real: ~a gc: ~a)" 'func (car results) cpu clock gc)))]))
 
+(define (time-proc/ns thunk [rounds 1])
+  (for/fold ([res #f]
+             [dur-lst '()]
+             #:result (values res (round (/ (apply + dur-lst) rounds))))
+            ([i (in-range rounds)])
+    (when (> i 0)
+      (collect-garbage)
+      (collect-garbage)
+      (collect-garbage))
+    (let* ([start (nanotime)]
+           [results (thunk)]
+           [dur (- (nanotime) start)])
+      (values results (cons dur dur-lst)))))
+
+(define (fmt-ns ns)
+  (cond
+    [(> ns 1000000) (format "~a ms" (quotient ns 1000000))]
+    [(> ns 1000) (format "~a μs" (quotient ns 1000))]
+    [else (format "~a ns" ns)]))
+
 (define-syntax (check-answer/ns stx)
-  (syntax-case stx ()
-    [(_ func answer)
+  (syntax-parse stx
+    [(check-answer/ns proc:id answer:expr (~optional rounds:expr))
      #`(test-begin
-        (define-values (results ns)
-          (let* ([start (nanotime)]
-                 [results (func)]
-                 [dur (- (nanotime) start)])
-            (values results dur)))
+        (define-values (results avg-ns) (time-proc/ns proc (~? rounds 1)))
         #,(syntax/loc stx (check-equal? results answer))
-        (define duration (if (> ns 1000) (format "~a μs" (quotient ns 1000)) (format "~a ns" ns)))
-        (displayln (format "~a: ~a (~a)" 'func results duration)))]))
+        (displayln (format "~a: ~a (~a)" 'proc results (fmt-ns avg-ns))))]))
+
